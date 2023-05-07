f75d9280efc4a4518ffddc6c08a9a3ea719244d9
Matiaspq <130516306+Matiaspq@users.noreply.github.com>
2023-05-07 14:25:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from claseManejadorViajero import ManejadorClaseViajero
+if __name__== '__main__':
+    v=ManejadorClaseViajero()
+    v.cargaViajero()
+    v.mayorCantidaddeMillas()
+    v.acumularMillasadd()
+    v.canjearMillassub()
+    v.mostrar()
+    
